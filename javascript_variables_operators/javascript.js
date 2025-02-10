4de9a903721a4762c390c3e3f575b33bbaf4a9ff
Siggywiggy <kurmaadam@gmail.com>
2025-02-11 00:08:41 +0200

    practice js

---
+++ javascript_variables_operators/javascript.js
@@ -0,0 +1,17 @@
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+console.log(23 + 97)
+console.log(23 + 97 + 1888 + 3 + 2 + 123024)
+console.log((4 + 6 + 9) / 77)
+
+let a = 10
+console.log(a)
+a = 12
+console.log(a)
+
+let b = a * 7
+console.log(b)
