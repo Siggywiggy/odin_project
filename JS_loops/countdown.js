68cb22372bdd974328e51a9e4e44ac4f550d3810
Siggywiggy <kurmaadam@gmail.com>
2025-02-16 10:24:15 +0200

    loops practice

---
+++ JS_loops/countdown.js
@@ -0,0 +1,20 @@
+const output = document.querySelector('.output');
+output.textContent = "";
+
+// loop from 10 down to 0 
+
+for (let i = 10; i >= 0; i-- ) {
+  // create a new <p> element
+  const para = document.createElement('p');
+  if (i == 10) {
+    para.textContent = "Counting down from 10!";
+  } else if (i == 0) { 
+    para.textContent = "Blastoff!";
+  } else { 
+    para.textContent = i;
+  }
+  // append para to output
+  output.appendChild(para);
+}
+
+
---
+++ JS_loops/guest_list.js
@@ -0,0 +1,19 @@
+const people = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
+
+const admitted = document.querySelector('.admitted');
+const refused = document.querySelector('.refused');
+admitted.textContent = 'Admit: ';
+refused.textContent = 'Refuse: ';
+
+// loop starts here
+
+for (const name of people) {
+    if ((name === "Phil") || (name === "Lola")) {
+        refused.textContent += name;
+    } else {
+        admitted.textContent += name;
+    }
+}
+
+admitted.textContent = admitted.textContent.slice(0, -2) + ".";
+refused.textContent = refused.textContent.slice(0, -2) + ".";
